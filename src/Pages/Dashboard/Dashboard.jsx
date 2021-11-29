8c6a23eb842b98a3ac17da72908f5feb43f31e72
hsa-98 <harshsanjayagrawal89@gmail.com>
2021-11-29 08:02:18 +0530

    feat: add delete note

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,13 +1,23 @@
 import Header from '../../Components/Header/Header'
 import CreateNote from '../../Components/CreateNote/CreateNote'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import Note from '../../Components/Note/Note';
-import { Grid } from '@material-ui/core';
 import { useEffect } from 'react';
 import { getNotes } from '../../Services/Note';
+import UpdateNote from '../../Components/UpdateNote/UpdateNote'
+import {deleteNotes} from '../../Services/Note.js'
 
 export default function DashBoard(){
+  const childRef = useRef();
   const [addData,setAddData] = useState([]);
+  const [update,setUpdate] = useState(false);
+
+  const [updateData,setUpdateData]=useState({
+    title:'',
+    description:'',
+    id:''
+  })
+ 
   useEffect(()=>{
     getNotes().then(response=>{
       const arr = response.data.data;
@@ -17,17 +27,41 @@ export default function DashBoard(){
       alert('Failed');
   })
   },[])
+
   const addNote = (data)=>{
     setAddData((prevData)=>{
       return[data,...prevData];
     })
-    console.log(data);
+  }
+ 
+  const updateNote = (info)=>{
+  
+    setUpdateData({
+      title:info.val.title,
+      description:info.val.description,
+      id:info.val._id
+    })
+    setUpdate(true);
+    childRef.current.handleOpen();
+  }
+
+  const deleteNote = (info)=>{
+    console.log(info);
+
+    deleteNotes(info.id).then(response=>{
+      const newNotes = addData.filter(note => note._id !== info.id);
+      setAddData(newNotes);
+    
+    }).catch(err=>console.log(err))
+
   }
   return(
     <>
       <Header/>
       <CreateNote  passNote={addNote}/>
-     <Note data={addData}></Note>
+     <Note addData={addData} updateNote={updateNote} deleteNote = {deleteNote}></Note>
+     {update}
+     <UpdateNote updateData={updateData} ref={childRef}/>
     </>
   )
 }
